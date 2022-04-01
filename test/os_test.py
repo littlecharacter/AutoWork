77228148a07c176f0f20e01d223c3a19e60e3bc1
LittleCharacter <littlecharacter@qq.com>
2022-04-01 16:50:21 +0800

    auto work

--- test/os_test.py
+++ test/os_test.py
@@ -1,13 +1,21 @@
 import os
+import psutil
 import time
 import subprocess
 
 
 if __name__ == "__main__":
     pass
-    op_content = "/Applications/微信.app"
+    # op_content = "/Applications/微信.app"
     # p = subprocess.Popen(op_content)
     # time.sleep(3)
     # p.terminate()
-    os.system(f'open \"{op_content}\"')
+    # os.system(f'open \"{op_content}\"')
     # os.system("osascript -e 'tell application \"/Applications/微信.app\" to quit'")
+
+    for pid in psutil.pids():
+        p = psutil.Process(pid)
+        print(p.name())
+        if p.name() == 'War3.exe':
+            print(p.name())
+            p.terminate()
